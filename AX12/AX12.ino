d90da845fee29acd760765302ea3b6260a677aa4
Adrien de Tocqueville <adrien.detocqueville@gmail.com>
2019-03-25 19:35:07 +0100

    macro for ax12 number

--- AX12/AX12.ino
+++ AX12/AX12.ino
@@ -1,14 +1,16 @@
 #include <DynamixelSerial.h>
 
+#define AX12 4
+
 void setup(){
    Dynamixel.begin(1000000,2);  // Initialize the servo at 1Mbps and Pin Control 2
 }
 
 void loop(){
-    Dynamixel.move(1,0);
+    Dynamixel.move(AX12,0);
     delay(2000);
-    Dynamixel.move(1,500);
+    Dynamixel.move(AX12,500);
     delay(2000);
-    Dynamixel.move(1,1000);
+    Dynamixel.move(AX12,1000);
     delay(2000);
 }
